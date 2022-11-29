ab689055036a8cfbcb968e5abb02cad5a996a258
Marcus Andersson <marcus@apparant.se>
2022-11-29 15:20:56 +0100

    Skapat grundstruktur + Guestbook-klass

---
+++ Moment 3/Guestbook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Moment_3
+{
+    internal class Guestbook
+    {
+        //Fält/medlemsvariabler för att lagra nödändig data. Private för att bara vara åtkomlig inom klassen och inte i hela programmet
+        private string filename;
+        private string path;
+        private string filePath;
+
+        public Guestbook(string filename) //Konstruktor, tänker att det kan bli möjligt att öppna olika gästböcker med olika namn i framtiden
+        {
+            this.filename = filename;
+            this.path = Directory.GetCurrentDirectory().ToString();
+            this.filePath = this.path + this.filename;
+        }
+
+        public bool saveGuestbook(string data)
+        {
+            if(filePath != null) //Kontrollera att filsökvägen inte är null
+            {
+                FileStream f = File.Open(this.filePath, FileMode.OpenOrCreate); //Öppna fil, om den inte finns så skapa den.
+                //f.WriteAsync("", default);
+                return true; //True, metoden lyckades skriva till fil
+            }
+            else
+            {
+                Console.WriteLine("Error: FilePath är null."); //Skriv ut felmeddelande   
+            }
+            return false; //False, metoden misslyckades att skriva till fil
+
+        }
+
+        public string readGuestbook()
+        {
+            //JsonConverter.
+            var guestbookStr = "TEMP: LÄSER UT HELA GUESTBOOK!"; //För testning
+            return guestbookStr;
+        }
+    }
+}
--- Moment 3/Program.cs
+++ Moment 3/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace Moment_3
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moment_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-----Ändrar lite utseende, bara för roligt
+            Console.Title = "Marcus Gästbok"; 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            bool exit = false; //Boolean för att avgöra om programmet ska avslutas eller inte
+            while (exit == false) //Kör loopen så länge inte användaren vill avsluta programmet
+            {
+                Guestbook myGuestbook = new Guestbook("marcusGuestbook.json"); //Instansiera Guestbook och öppna marcusGuestbook.json
+                string? pressedKey = ""; //string? för att hantera felmeddelande om possible null
+                Console.WriteLine("MARCUS GÄSTBOK!");
+                Console.WriteLine("\n \n Meny: \n\t[1] Skriv ett nytt inlägg\n\t[2] Ta bort ett inlägg \n \n\t[X] Avsluta programmet");
+
+
+                pressedKey = Console.ReadLine();
+                
+                switch(pressedKey)
+                {
+                    case "1": //Skriv nytt inlägg
+                        Console.Clear(); //Rensa skärmen
+                        //Fråga efter Input Nytt namn på författare + inlägg
+
+                        //Kontrollera Input
+
+                        //Spara inlägg och visa meddelande om det ELLER skriv ut felmeddelande
+
+                    break;
+
+                    case "2": //Ta bort inlägg
+                        Console.Clear(); //Rensa skärmen
+
+                        //Skriv ut alla inlägg för att visa Index
+
+                        //Fråga efter Input om valt Index
+
+                        //Kontrollera input
+
+                        //Radera valt index eller skriv ut felmeddelande
+
+
+                    break;
+                    case "x": //Avsluta, oavsett om anv. skriver litet x eller X
+                    case "X":
+                        Console.Clear(); //Rensa skärmen
+                        exit = true; //Användaren vill avsluta, sätt exit till true
+                    break;
+                    default: //Om inte 1, 2 eller X/x så har användaren gjort ett felaktigt val.
+                        Console.Clear(); //Rensa skärmen
+                        Console.WriteLine("\n\tFelaktigt val! Försök igen\n\n\a"); //\a = spela upp "alert"-ljud
+                    break;
+                }
+
+                myGuestbook.readGuestbook();
+            }
         }
     }
 }
